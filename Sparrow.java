6369f4665904353b1b1c9b0311ed08c556931ea2
RowyMonke <nattakritta_ao@cmu.ac.th>
2025-09-04 01:59:08 +0700

    Update Sparrow.java

--- Sparrow.java
+++ Sparrow.java
@@ -1,4 +1,4 @@
-package oop.ocp;
+package oop.lsp;
 
 public class Sparrow implements Flyable{
     //interface Flyable() extends from interface Moveable(), so Sparrow() class can using it too
@@ -12,3 +12,4 @@ public class Sparrow implements Flyable{
         System.out.println("Look! The (Jack) Sparrow flies high in the sky.");
     }
 }
+
